9d51b2edb09d8e43a3ecda04dc07839d38cb77c6
Deirdre Storck <deirdre.storck@gmail.com>
2016-07-12 14:39:40 -0700

    added some models and migrations for basic setup

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,22 @@ Rails.application.routes.draw do
   root 'ranker#index'
 
   get     'movies'            => 'movies#index'
-  get     'movies/:id'        => 'movies#show'    as: :movie
+  get     'movies/:id'        => 'movies#show',    as: :movie
   delete  'movies/:id'        => 'movies#delete'
   get     'movies/:id/edit'   => 'movies#edit',   as: :edit_movie
   patch   'movies/:id/update' => 'movies#update', as: :update_movie
+
+  get     'books'            => 'books#index'
+  get     'books/:id'        => 'books#show',    as: :book
+  delete  'books/:id'        => 'books#delete'
+  get     'books/:id/edit'   => 'books#edit',   as: :edit_book
+  patch   'books/:id/update' => 'books#update', as: :update_book
+
+  get     'albums'            => 'albums#index'
+  get     'albums/:id'        => 'albums#show',    as: :album
+  delete  'albums/:id'        => 'albums#delete'
+  get     'albums/:id/edit'   => 'albums#edit',   as: :edit_album
+  patch   'albums/:id/update' => 'albums#update', as: :update_album
 
 
 end
--- db/migrate/20160712211528_create_albums.rb
+++ db/migrate/20160712211528_create_albums.rb
@@ -1,6 +1,10 @@
 class CreateAlbums < ActiveRecord::Migration[5.0]
   def change
     create_table :albums do |t|
+      t.string :name
+      t.string :artist
+      t.string :description
+      t.integer :rating
 
       t.timestamps
     end
