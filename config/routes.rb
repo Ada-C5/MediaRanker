d7aaa2474964da0c4b9ae54b6b511957a52d9fc7
Deirdre Storck <deirdre.storck@gmail.com>
2016-07-12 14:30:52 -0700

    made prelim routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,28 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'ranker#index'
+
+  get     'movies'            => 'movies#index'
+  get     'movies/:id'        => 'movies#show'    as: :movie
+  delete  'movies/:id'        => 'movies#delete'
+  get     'movies/:id/edit'   => 'movies#edit',   as: :edit_movie
+  patch   'movies/:id/update' => 'movies#update', as: :update_movie
+
+
 end
+
+
+
+
+
+
+
+
+# root 'search#index'
+#   post 'search' => 'search#display_events', as: :display_events
+#   get '/auth/:provider/callback' => 'sessions#create'
+#   delete "/logout" => "sessions#destroy"
+#
+#   get 'users/:id' => 'users#profile' , as: :profile
+#   get 'users/:id/edit' => 'users#edit_profile', as: :edit_profile
+#   patch 'users/:id' => "users#update_profile", as: :update_profile
