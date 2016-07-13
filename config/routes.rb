19d58de99f7b028134c39fd0a4d8db4656afa723
Rowan Cota <cota.rg@gmail.com>
2016-07-13 10:59:37 -0700

    bundled and boilerplated.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'application#index'
+
+  resources :movies
+  resources :albums
+  resources :books
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
