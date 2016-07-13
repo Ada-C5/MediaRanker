c0563c205b6f65af36251d31706e33d44b7fdaa4
Jade Vance <jadenicolevance@gmail.com>
2016-07-13 10:16:17 -0700

    Generating all of the models, all of the controllers and all of the routes

---
+++ config/routes.rb
@@ -0,0 +1,21 @@
+Rails.application.routes.draw do
+  root                        to: "home#index"
+  get     'movies',           to: 'movies#index'
+  get     'movies/:id',       to: 'movies#show'
+  get     'movies/:id/edit',  to: 'movies#edit'
+  patch   'movies/:id',       to: 'movies#update'
+  delete  'movies/:id',       to: 'movies#destroy' 
+
+  get     'books',            to: 'books#index'
+  get     'books/:id',        to: 'books#show'
+  get     'books/:id/edit',   to: 'books#edit'
+  patch   'books/:id',        to: 'books#update'
+  delete  'books/:id',        to: 'books#destroy' 
+
+  get 'albums',               to: 'albums#index'
+  get 'albums/:id',           to: 'albums#show'
+  get 'albums/:id/edit',      to: 'albums#edit'
+  patch 'albums/:id',         to: 'albums#update'
+  delete 'albums/:id',        to: 'albums#destroy' 
+end
+
