36ec1469177555f850d03cc4571222d05a5b1cc4
Anna Wilson <annie.marie.mason@gmail.com>
2016-07-13 20:43:14 -0700

    seed file created and it works too

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# rake db:seed
+
+album_list = [
+  { name: "Crazy In Love", artist: "Beyonce", description: "She's really in love.", votes: 10 },
+  { name: "Purple Rain", artist: "Prince", description: "80s power ballads.", votes: 1 },
+  { name: "Thriller", artist: "Michael Jackson", description: "", votes: 3 },
+  { name: "Channel Orange", artist: "Frank Ocean", description: "lovesit.", votes: 0 }
+]
+
+album_list.each do |album|
+  Album.create(album)
+end
+
+puts "#{Album.count} albums added."
+
+book_list = [
+  { name: "Harry Potter", author: "J.K. Rowling", description: "obvi", votes: 10},
+  { name: "Digital Fortress", author: "Dan Brown", description: "my husband likes this book", votes: 0},
+  { name: "Another Book", author: "Idont Readenough", description: "", votes: 2}
+]
+
+book_list.each do |book|
+  Book.create(book)
+end
+
+puts "#{Book.count} books added."
+
+movie_list = [
+  {name: "Titanic", director: "Some Guy", description: "Super sad tho", votes: 3},
+  {name: "You've Got Mail", director: "Dunno", description: "This is from the 90s I hear", votes: 0}
+]
+
+movie_list.each do |movie|
+  Movie.create(movie)
+end
+
+puts "#{Movie.count} movies added."
