155be7704f3513e5595055ab31d168de319470d1
sophia <sophiabaldonado@gmail.com>
2016-07-14 14:54:30 -0700

    Display book info on index view;

--- controllers/books.js
+++ controllers/books.js
@@ -5,6 +5,7 @@ var BooksController = {
   index: function (req, res, next) {
 
     Book.all(function(error, books) {
+      // console.log(books[0].name)
       if(error) {
         var err = new Error("Error retrieving book list:\n" + error.message)
         err.status = 500
@@ -12,7 +13,7 @@ var BooksController = {
       } else {
         locals = {}
         locals.books = books
-        res.render ('books/index', locals.books)
+        res.render ('books/index', { locals: locals })
       }
     })
   }
