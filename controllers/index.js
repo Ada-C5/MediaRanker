91c2812086aa42c63bdaafadc45e8fdbe07d67b1
Leah <leahnpetersen@gmail.com>
2016-07-14 11:09:16 -0700

    added callbacks to get info from books, albums and movies db

--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,47 @@ var Books = require("../models/books_model");
 var Albums = require("../models/albums_model");
 var Movies = require("../models/movies_model");
 
+// var Books = require("../controllers/books.js");
+// var Albums = require("../controllers/albums.js");
+// var Movies = require("../controllers/movies.js");
+
 var IndexController = {
+	// get top 10 media with ranking, upvotes and title in desc order
+	// getIndex: function(req, res) {
+	// 	res.render('index', { title: 'Media Ranker' });
+	// }
+
 	getIndex: function(req, res) {
-		res.render('index', { title: 'Media Ranker' });
-	}
+		Movies.all(function(error, topMovies) {
+			if(error) {
+				var err = new Error("Could not retrieve movies");
+				err.status = 404;
+			} else {
+				Albums.all(function(error, topAlbums) {
+					if(error) {
+						var err = new Error("Could not retrieve albums");
+						err.status = 404;
+					} else {
+						Books.all(function(error, topBooks) {
+							if(error) {
+								var err = new Error("Could not retrieve books");
+								err.status = 404;
+							} else {
+								console.log(topBooks)
+									res.render('index', { 
+														movies: topMovies,
+														books: topBooks,
+														albums: topAlbums
+												});
+											}
+									});
+							}
+					});
+				}
+			})
+		}
 }
 
 module.exports = IndexController
+
+
--- routes/index.js
+++ routes/index.js
@@ -2,24 +2,7 @@ var express = require('express');
 var router = express.Router();
 var Controller = require('../controllers/index')
 
-/* GET docs json. */
-// router.get('/api/docs.json', Controller.getJsonDocs)
-
-/* GET docs html page. */
-// router.get('/api/docs', Controller.getDocs)
-
-
 /* GET home page. */
 router.get('/', Controller.getIndex)
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Scrabble Express' });
-// });
-
 
 module.exports = router;
-
-
-// 	{ 
-// 	title: 'Movies List',
-// 	movies: movies
-// }
