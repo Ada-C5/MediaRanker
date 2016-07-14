1551b70ecda433f95e4ea5102bba95dc6019cdbb
Lisa Rolczynski <lisa.sano@gmail.com>
2016-07-14 14:12:55 -0700

    added columns for title, creator, description, and upvotes to the movie, album, and book models. migrated to update the schema

--- db/migrate/20160714205314_create_movies.rb
+++ db/migrate/20160714205314_create_movies.rb
@@ -1,6 +1,10 @@
 class CreateMovies < ActiveRecord::Migration[5.0]
   def change
     create_table :movies do |t|
+      t.string :title
+      t.string :director
+      t.string :description
+      t.integer :upvotes
 
       t.timestamps
     end
