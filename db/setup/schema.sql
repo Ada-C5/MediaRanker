9c054fd6f71164e41645cee382a4bcfc29469aaf
sophia <sophiabaldonado@gmail.com>
2016-07-13 22:13:55 -0700

    DB Schema

---
+++ db/setup/schema.sql
@@ -0,0 +1,26 @@
+DROP TABLE IF EXISTS movies;
+CREATE TABLE movies(
+  id serial PRIMARY KEY,
+  name text,
+  director text,
+  description text,
+  votes integer
+);
+
+DROP TABLE IF EXISTS albums;
+CREATE TABLE albums(
+  id serial PRIMARY KEY,
+  name text,
+  author text,
+  description text,
+  votes integer
+);
+
+DROP TABLE IF EXISTS books;
+CREATE TABLE books(
+  id serial PRIMARY KEY,
+  name text,
+  artist text,
+  description text,
+  votes integer
+);
