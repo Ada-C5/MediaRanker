0bb8aecc8ebadd473c275355b7273b8e92758805
sophia <sophiabaldonado@gmail.com>
2016-07-14 15:19:23 -0700

    Fix views;

--- controllers/albums.js
+++ controllers/albums.js
@@ -4,15 +4,15 @@ var AlbumsController = {
   index: function (req, res, next) {
 
     Album.all(function(error, albums) {
-      // console.log(albums[0].name)
       if(error) {
         var err = new Error("Error retrieving book list:\n" + error.message)
         err.status = 500
         next(err)
       } else {
         locals = {}
+        locals.type = "Albums"
         locals.media = albums
-        res.render ('books/index', { locals: locals })
+        res.render ('index', { locals: locals })
       }
     })
   }
--- controllers/books.js
+++ controllers/books.js
@@ -5,15 +5,15 @@ var BooksController = {
   index: function (req, res, next) {
 
     Book.all(function(error, books) {
-      // console.log(books[0].name)
       if(error) {
         var err = new Error("Error retrieving book list:\n" + error.message)
         err.status = 500
         next(err)
       } else {
         locals = {}
+        locals.type = "Books"
         locals.media = books
-        res.render ('books/index', { locals: locals })
+        res.render ('index', { locals: locals })
       }
     })
   }
