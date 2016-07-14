7fb3ce5fccfaac5671c1ba3d0dbc78943e95a919
sophia <sophiabaldonado@gmail.com>
2016-07-14 10:36:10 -0700

    Book model all function;

---
+++ models/book.js
@@ -0,0 +1,25 @@
+var app = require("../app");
+var db = app.get("db");
+
+// Constructor function
+var Book = function(book) {
+  this.id = book.id
+  this.name = book.name
+  this.description = book.description
+  this.author = book.author
+  this.votes = book.votes
+}
+
+Book.all = function(callback) {
+  db.run("SELECT * FROM books", function(error, books) {
+    if(error || !movies) {
+      callback(error || new Error("Could not retrieve books"), undefined)
+    } else {
+      callback(null, books.map(function(book) {
+        return new Book(book)
+      }))
+    }
+  })
+}
+
+module.exports = Book
