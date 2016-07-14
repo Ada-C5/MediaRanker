ed503c8d579b875656ac51661f6a07e8d95c10f8
sophia <sophiabaldonado@gmail.com>
2016-07-14 10:35:45 -0700

    Books controller index function;

--- controllers/books.js
+++ controllers/books.js
@@ -2,6 +2,12 @@ var Book = require("../models/book")
 
 var BooksController = {
 
+  index = function () {
+    locals = Book.all
+
+    render ('books/index', locals)
+  }
+
 }
 
 module.exports = BooksController
