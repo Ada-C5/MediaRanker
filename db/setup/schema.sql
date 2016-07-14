9eb958f3e2a6842e3fceedec9aac3af703d7febf
Cristal Tay <bluecolorpencils@gmail.com>
2016-07-13 22:01:00 -0700

    commiting is for losers. jk. added express, jasmine and istanbul. npm installed. then set up the mvc model. currently adding seeded data and schema.

---
+++ db/setup/schema.sql
@@ -0,0 +1,29 @@
+DROP TABLE IF EXISTS albums;
+DROP TABLE IF EXISTS movies;
+DROP TABLE IF EXISTS books;
+
+CREATE TABLE albums(
+  id serial PRIMARY KEY,
+  name text,
+  director text,
+  description text,
+  votes integer
+);
+
+CREATE TABLE movies(
+  id serial PRIMARY KEY,
+  name text,
+  author text,
+  description text,
+  votes integer
+);
+
+
+CREATE TABLE books(
+  id serial PRIMARY KEY,
+  name text,
+  author text,
+  description text,
+  votes integer
+
+);
