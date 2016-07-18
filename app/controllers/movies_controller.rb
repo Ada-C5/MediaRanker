f83d9fd09bddd286b0625327e7df78d4b8cb6ad0
Lisa Rolczynski <lisa.sano@gmail.com>
2016-07-18 12:23:25 -0700

    added an edit page and a form partial. bootstrap styling. edit and update paths added to movies controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,9 +7,24 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params[:id])
   end
 
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    Movie.update(params[:id], movies_params[:movie])
+    redirect_to movies_path
+  end
+
   def upvote
     @movie = Movie.find(params[:id])
     @movie.upvote_movie
     redirect_to movie_path(params[:id])
+  end
+
+  private
+
+  def movies_params
+    params.permit(movie: [:title, :director, :description])
   end
 end
