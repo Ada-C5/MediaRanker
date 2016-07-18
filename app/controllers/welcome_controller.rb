a49b67cc2887b5a24b371c1aee805a0fc58b0d58
Lisa Rolczynski <lisa.sano@gmail.com>
2016-07-18 11:42:51 -0700

    make an index page that shows the top 10 movies, books, and albums. styled using bootstrap and css

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,7 @@
 class WelcomeController < ApplicationController
+  def index
+    @movies = Movie.limit(10).order(upvotes: :desc)
+    @books = Book.limit(10).order(upvotes: :desc)
+    @albums = Album.limit(10).order(upvotes: :desc)
+  end
 end
