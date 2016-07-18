62698777f0418f3385aaf1ba407a7a37ebc64e10
melissajimison <melissajimison@gmail.com>
2016-07-18 01:11:16 -0700

    edit methos working crispy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,18 +22,30 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
-  def new
-  end
-
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
     redirect_to root_path
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def update
     @article = Article.find(params[:id])
-    @article.update(ranking: params[:article][:ranking])
-    redirect_to root_path
+    if params[:article][:ranking]
+      @article.update(ranking: params[:article][:ranking])
+      redirect_to root_path
+    else
+      @article.update(market_update_params)
+      redirect_to article_path
+    end
+  end
+
+  private
+
+  def market_update_params
+    params.require(:article).permit(:title, :author, :description, :photo)
   end
 end
