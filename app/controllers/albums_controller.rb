ed26dadc76fea2f685eb71c1c60104fffd15e4f2
Lisa Rolczynski <lisa.sano@gmail.com>
2016-07-18 13:48:45 -0700

    index, show, new, edit, and update functionality copied from movies

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,2 +1,47 @@
 class AlbumsController < ApplicationController
+  def index
+    @albums = Album.order(upvotes: :desc)
+  end
+
+  def show
+    @album = Album.find(params[:id])
+  end
+
+  def new
+    @album = Album.new
+  end
+
+  def create
+    @album = Album.create(albums_params[:album])
+    @album.upvotes = 0
+    @album.save
+    redirect_to albums_path
+  end
+
+  def edit
+    @album = Album.find(params[:id])
+  end
+
+  def update
+    Album.update(params[:id], albums_params[:album])
+    redirect_to albums_path
+  end
+
+  def destroy
+    @album = Album.find(params[:id])
+    @album.destroy
+    redirect_to albums_path
+  end
+
+  def upvote
+    @album = Album.find(params[:id])
+    @album.upvote
+    redirect_to albums_path
+  end
+
+  private
+
+  def albums_params
+    params.permit(album: [:title, :artist, :description])
+  end
 end
