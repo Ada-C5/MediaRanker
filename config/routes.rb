a6badb4adf8fc5b9a8d04b2b6dcc2ed4937f9f9b
melissajimison <melissajimison@gmail.com>
2016-07-17 22:26:45 -0700

    Got rid off the Welcome routes. Created the Resouceful routes for articles, and added the books, movies, and albums

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
-  get '/welcome' => 'welcome#index', as: 'welcome'
+
+  root 'articles#index'
+  get '/movies' => 'articles#movies', as: 'movies'
+  get '/books' => 'articles#books', as: 'books'
+  get '/albums' => 'articles#albums', as: 'albums'
+
+  resources :articles
+
 
 end
