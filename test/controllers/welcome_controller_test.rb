a240e90962dfab867925f6f1f55fa424bef8feb4
Lisa Rolczynski <lisa.sano@gmail.com>
2016-07-18 14:39:12 -0700

    controller testing for welcome controller (root path)

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class WelcomeControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get root path" do
+    get '/'
+    assert_response :success
+    assert_not_nil assigns(:movies)
+    assert_not_nil assigns(:books)
+    assert_not_nil assigns(:albums)
+  end
 end
