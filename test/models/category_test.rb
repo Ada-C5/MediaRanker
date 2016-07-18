54e09958f36397c0288f47cd1440d3393b336c60
melissajimison <melissajimison@gmail.com>
2016-07-18 16:00:07 -0700

    Category model test passing

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  describe "Category" do
+    it "knows that there are three categories" do
+      assert_equal 3, Category.all.count
+    end
+
+    it "knows that categories have 'name'" do
+      assert_equal "movies", Category.first.name
+      assert_equal "albums", Category.last.name
+    end
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,17 @@
+require 'simplecov'
+SimpleCov.start do
+  add_filter "/test/"
+  add_filter "/app/helpers/"
+  add_filter "/config/environment.rb"
+  add_filter "/config/environments/"
+
+end
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'minitest/spec'
+require "minitest/reporters"
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
