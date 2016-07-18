0457ced219a0365fa4203049802811bd681e2fcf
melissajimison <melissajimison@gmail.com>
2016-07-18 11:56:37 -0700

    edit and the two cases update tested

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -16,10 +16,30 @@ class ArticlesControllerTest < ActionController::TestCase
   #   get :destroy
   #   assert_response :success
   # end
-  #
-  # test "should get update" do
-  #   get :update
-  #   assert_response :success
-  # end
+
+  test "should get edit" do
+
+    id = Article.first.id
+    get :edit, id: id
+    assert_response :success
+
+  end
+
+  test "should get update an article" do
+    id = Article.first.id
+    patch :update, id: id, article: { title: "Maria llena eres de gracia", description: "This is a test", author: "Awsome Programmer" }
+    assert_redirected_to article_path(id)
+  end
+
+  test "should get update an upvote" do
+    id = Article.first.id
+    params = {
+      article: {
+        ranking: 2
+      }
+    }
+    patch :update, id: id, article: { ranking: params[:article][:ranking] }
+    assert_redirected_to root_path(id)
+  end
 
 end
