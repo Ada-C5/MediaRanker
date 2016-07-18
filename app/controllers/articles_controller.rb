cdbc8c36251b940564fd9ab205c396a41042f885
melissajimison <melissajimison@gmail.com>
2016-07-18 11:39:34 -0700

    index and show tests for article controller passing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,7 +20,7 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
-
+    @category = Category.find(@article.category_id)
     if @article.category_id == 3
       @spotify = RSpotify::Album.search(@article.title)[0].id
     end
--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -7,23 +7,19 @@ class ArticlesControllerTest < ActionController::TestCase
   end
 
   test "should get show" do
-    get :show
+    article = Article.first
+    get :show, {id: article.id}
     assert_response :success
   end
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should get delete" do
-    get :destroy
-    assert_response :success
-  end
-
-  test "should get update" do
-    get :update
-    assert_response :success
-  end
+  # test "should get delete" do
+  #   get :destroy
+  #   assert_response :success
+  # end
+  #
+  # test "should get update" do
+  #   get :update
+  #   assert_response :success
+  # end
 
 end
