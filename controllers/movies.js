d8c5432308098aee99678955541c2f63f3423c45
sophia <sophiabaldonado@gmail.com>
2016-07-19 15:38:44 -0700

    Show action for Movies;

--- controllers/movies.js
+++ controllers/movies.js
@@ -19,7 +19,7 @@ var MoviesContoller = {
   },
 
   show: function (req, res, next) {
-    var id = 3
+    var id = req.params.movie_id
 
     Movie.find(id, function(error, movies) {
       if(error) {
@@ -29,16 +29,15 @@ var MoviesContoller = {
       } else {
         var locals = {}
         locals.type = "Movies"
-        locals.media = movies
-        res.render ('index', { locals: locals })
+        locals.media = movies[0]
+        console.log("boop: ", movies)
+        res.render ('movies/show', { locals: locals })
       }
     })
   },
 
   upvote: function (req, res, next) {
-    console.log("goop: ", req.params)
-    // var movie_id = req.params.movie_id
-    var movie_id = 1
+    var movie_id = req.body.upvote
 
     Movie.addVote(movie_id, function(error, moe_id) {
       if(error) {
@@ -46,9 +45,6 @@ var MoviesContoller = {
         err.status = 500
         next(err)
       } else {
-        var locals = {}
-        locals.type = "Movies"
-        locals.id = moe_id
         res.redirect (200, '/movies/' + movie_id)
       }
     })
