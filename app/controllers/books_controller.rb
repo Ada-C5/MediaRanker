60d08fadecfccb886188446cd4664139eb0f10c4
Jessica <jessica.weeber@gmail.com>
2016-07-20 16:09:49 -0700

    added partial for rendering each media's index page. changed routes to include all resources for adding new media

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,39 @@
 class BooksController < ApplicationController
+
+  def index
+    @books = Book.order(rank: :desc).all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id]) 
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update_attributes(book_update_params[:book])
+    redirect_to book_path(@book.id)
+  end
+
+  def upvote
+    @book = Book.find(params[:id])
+    ranking = @book.rank
+    @book.update(rank: ranking += 1)
+    redirect_to book_path(@book.id) 
+  end
+
+  def destroy
+    @book = Book.find(params[:id]).delete
+    redirect_to books_path
+  end
+
+  private
+
+  def book_update_params
+    params.permit(book: [:title, :directed_by, :description])
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ Rails.application.routes.draw do
  
  root "home#index"
 
- resources :movies, :except => [:new, :create]
+ resources :movies
  put "movies/:id/upvote" => "movies#upvote", as: :movie_upvote
 
- resources :books, :except => [:new, :create]
- resources :albums, :except => [:new, :create]
+ resources :books
+  put "books/:id/upvote" => "books#upvote", as: :book_upvote
+
+ resources :albums
+ put "albums/:id/upvote" => "albums#upvote", as: :album_upvote
 
 end
