b0b479fe0125a21b92a0c0b2344988870024435e
Anna Wilson <annie.marie.mason@gmail.com>
2016-07-20 07:43:46 -0700

    added albums functionality

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,22 +1,51 @@
 class AlbumsController < ApplicationController
-  def new
-  end
 
-  def create
-  end
+    def index
+      @albums = Album.all.order('votes desc')
+    end
 
-  def update
-  end
+    def show
+      @album = Album.find(params[:id])
+    end
 
-  def edit
-  end
+    def new
+      @new_album = Album.new
+    end
 
-  def destroy
-  end
+    def create
+      @new_album = Album.new
+      @new_album.name = params[:album]["name"]
+      @new_album.artist = params[:album]["artist"]
+      @new_album.description = params[:album]["description"]
+      @new_album.votes = 0
+      @new_album.save
+      redirect_to album_path(@new_album.id)
+    end
 
-  def show
-  end
+    def edit
+      @album = Album.find(params[:id])
+    end
 
-  def index
-  end
+    def update
+      if params.key?("upvote")
+        @album = Album.find(params[:id])
+        @album.votes += 1
+        @album.save
+        redirect_to album_path(@album.id)
+      else
+        @album = Album.find(params[:id])
+        @album.name = params[:album]["name"]
+        @album.artist = params[:album]["artist"]
+        @album.description = params[:album]["description"]
+        @album.save
+        redirect_to album_path(@album.id)
+      end
+    end
+
+    def destroy
+      @album = Album.find(params[:id])
+      @album.destroy
+      redirect_to albums_path
+    end
+
 end
