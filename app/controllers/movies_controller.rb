5456e90c6207d4b4ac35e219313e0f344a6dedaa
Jessica <jessica.weeber@gmail.com>
2016-07-20 14:46:56 -0700

    edit path for movies now working. form created and updating information is saving to db.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,6 +8,16 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params[:id])
   end
 
+  def edit
+    @movie = Movie.find(params[:id]) 
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    @movie.update_attributes(movie_update_params[:movie])
+    redirect_to movie_path(@movie.id)
+  end
+
   def upvote
     @movie = Movie.find(params[:id])
     ranking = @movie.rank
@@ -19,4 +29,11 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params[:id]).delete
     redirect_to movies_path
   end
+
+  private
+
+  def movie_update_params
+    params.permit(movie: [:title, :directed_by, :description])
+  end
+  
 end
