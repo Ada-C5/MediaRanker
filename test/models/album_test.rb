1e0268f41815cc6c758301828dfbb02ef3560801
Anna Wilson <annie.marie.mason@gmail.com>
2016-07-20 15:47:11 -0700

    testing for model validations

--- test/models/album_test.rb
+++ test/models/album_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class AlbumTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should save a new instance of album with title" do
+    album = Album.new
+    album.name = "Test Album"
+    assert album.save
+  end
+
+  test "should not save a new instance of album without title" do
+    album = Album.new
+    assert_not album.save
+  end
 end
--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class MovieTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should save a new instance of movie with title" do
+    movie = Movie.new
+    movie.name = "Test Movie"
+    assert movie.save
+  end
+
+  test "should not save a new instance of movie without title" do
+    movie = Movie.new
+    assert_not movie.save
+  end
 end
