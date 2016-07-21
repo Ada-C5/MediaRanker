3d0540dcff8a72bad11a1dcdb3cd90cbce06eb67
Jade Vance <jadenicolevance@gmail.com>
2016-07-20 23:03:19 -0700

    cleaned up routes

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,8 @@ class BooksController < ApplicationController
   def index
     @books = Book.order(rank: :desc)
   end 
+
+  def show
+    @book = Book.find(params[:id])
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,23 @@
 Rails.application.routes.draw do
-  root                        to: "home#index"
-  get     'movies',           to: 'movies#index'
-  get     'movies/:id',       to: 'movies#show', as: :movie
-  get     'movies/:id/edit',  to: 'movies#edit'
-  patch   'movies/:id',       to: 'movies#update'
-  delete  'movies/:id',       to: 'movies#destroy' 
+  root "home#index"
 
-  get     'books',            to: 'books#index'
-  get     'books/:id',        to: 'books#show', as: :book
-  get     'books/:id/edit',   to: 'books#edit'
-  patch   'books/:id',        to: 'books#update'
-  delete  'books/:id',        to: 'books#destroy' 
+  resources :books do
+    member do
+      put 'upvote'
+    end
+  end
 
-  get     'albums',           to: 'albums#index'
-  get     'albums/:id',       to: 'albums#show', as: :album
-  get     'albums/:id/edit',  to: 'albums#edit'
-  patch   'albums/:id',       to: 'albums#update'
-  delete  'albums/:id',       to: 'albums#destroy' 
+  resources :movies do
+    member do
+      put 'upvote'
+    end
+  end
+
+  resources :albums do
+    member do
+      put 'upvote'
+    end
+  end
+
 end
 
