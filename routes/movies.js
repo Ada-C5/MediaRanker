143b6349965ebae63eef40ab81afcca2f20bd746
Leah <leahnpetersen@gmail.com>
2016-07-22 12:31:32 -0400

    updated movies routes

--- routes/movies.js
+++ routes/movies.js
@@ -1,15 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var Controller = require('../controllers/movies')
-
+var indexController = require('../controllers/index')
 
 router.get('/:id', Controller.getMovies)
-// select all from db
 router.get('/', Controller.getMovies)
-
-// router.get('/sort/:column', Controller.subsetCustomers)
-// router.get('/:customer_id/current', Controller.getCustomersCurrent)
-// router.get('/:customer_id/history', Controller.getCustomersHistory)
-
+router.post('/:id/delete', indexController.deleteMedia)
+router.post('/:id', indexController.upvote)
 
 module.exports = router;
