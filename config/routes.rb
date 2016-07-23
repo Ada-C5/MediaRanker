8b3a6e2f50e408024bbb1a96d3c658bb9f066f11
Nadine Curry <nlcurry@gmail.com>
2016-07-22 23:04:00 -0700

    added routes for upvote

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   root 'home#index'
 
-  resources :movies, :albums, :books
+  resources :movies do
+    put 'upvote'
+  end
 
+  resources :albums do
+    put 'upvote'
+  end
+
+  resources :books do
+    put 'upvote'
+  end
 end
