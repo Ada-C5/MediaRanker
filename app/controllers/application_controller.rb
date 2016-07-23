abe50906822053bc873f9ebb4373fc53a8a088b4
Nadine Curry <nlcurry@gmail.com>
2016-07-22 19:33:26 -0700

    index funtion for homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def index
+    @movies = Movie.limit(10).order('upvote desc')
+    @books = Book.limit(10).order('upvote desc')
+    @albums = Album.limit(10).order('upvote desc')
+  end
 end
