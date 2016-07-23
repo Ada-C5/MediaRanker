03ba5000727d55d9944d97a4a6f64484f6005c39
Suzanne Harrison <harrison_family@hotmail.com>
2016-07-23 15:55:25 -0700

    index now displays ranks correctly--working on model tests.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
   def index
-    @movies = Movie.all
-    @albums = Album.all
-    @books = Book.all
+    @movies = Movie.all.order('rank desc')
+    @albums = Album.all.order('rank desc')
+    @books = Book.all.order('rank desc')
   end
 end
