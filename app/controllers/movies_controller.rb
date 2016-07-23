3e157fe407980c8dc3fb5adc1ab44ea2db9fd338
Ania Gonzalez <aniagr@gmail.com>
2016-07-23 01:22:42 -0500

    Movies can be edited

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,4 +14,26 @@ class MoviesController < ApplicationController
     redirect_to movies_path
   end
 
+  def edit
+    @movie = Movie.find(params[:movie_id])
+    render :new
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    @movie.update(movie_update_params[:movie])
+    redirect_to movie_path
+  end
+
+  private
+
+  def movie_create_params  #tell rails which params are ok to be in the model
+    params.permit(movie: [:name, :director, :description])
+  end
+
+  def movie_update_params  #tell rails which params are ok to be in the model
+    params.permit(movie: [:name, :director, :description])
+  end
+
+
 end
