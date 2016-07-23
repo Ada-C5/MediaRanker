8949a81273aed05c566e84b00d04d02242710c43
Jade Vance <jadenicolevance@gmail.com>
2016-07-22 17:23:41 -0700

    Remembering how to yaml and make fixtures for testing

--- test/controllers/albums_controller_test.rb
+++ test/controllers/albums_controller_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class AlbumsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  
+
+  test "should get index" do
+    get '/albums'
+    assert_response :success
+  end
 end
